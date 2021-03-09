07ea714793f96a0e7a9d1e37f91d02c845660aaa
John <jbartlettdesign@gmail.com>
2021-03-09 14:57:01 -0600

    update

--- assets/js/games.js
+++ assets/js/games.js
@@ -63,7 +63,16 @@ var fight = function(enemyName) {
   };
 
   for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators!" + ( i + 1 ));
+      
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
+    debugger;
     fight(pickedEnemyName);
   }
+  else {
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  }
+}
